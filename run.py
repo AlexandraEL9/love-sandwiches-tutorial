907d2615a8b54853e3fe0f72da30b67ab16a579a
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-06-10 13:25:58 +0000

    add validation for data type

--- run.py
+++ run.py
@@ -40,18 +40,18 @@ def get_sales_data():
 #validation function
 def validate_data(values):
     """
-    inside the try, convert all string values to integers,
-    raises valueErrot if string cannot be converted to integer,
-    or if there aren't exactly 6 values
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: (e), please try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
 
-#call function outside of the function
+
 get_sales_data()
-#can enter data into the terminal and then terminal reiterates back
