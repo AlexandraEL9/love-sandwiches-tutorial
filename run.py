928b5ad676f71a04d65b82241886340152690935
Alexandra Lee <alexandraelee9@gmail.com>
2024-06-20 12:35:53 +0000

    reinstall libraries

--- run.py
+++ run.py
@@ -1,75 +1,30 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Define the scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
+# Provide the path to your service account key file
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-"""
-check linked up- Yes
+# Apply the defined scopes to the credentials
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+# Authorize the client using the scoped credentials
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Open the Google Sheets file by name
+SHEET = GSPREAD_CLIENT.open('love-sandwiches')
+
+# Access the 'sales' worksheet
 sales = SHEET.worksheet('sales')
 
+# Fetch all data from the worksheet
 data = sales.get_all_values()
 
+# Print the fetched data
 print(data)
-"""
-def get_sales_data():
-    """
-    Get sales figures input from the user.
-    """
-    while True:
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
-
-        data_str = input("Enter your data here: ")
-
-        sales_data = data_str.split(",")
-
-        if validate_data(sales_data):
-            print("Data is valid!")
-            break
-
-    return sales_data
-
-
-def validate_data(values):
-    """
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-    return True
-
-def update_sales_worksheet(data):
-    """
-    update sales worksheet'
-    add new row with the data provided by the user input
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully!\n")
-
-
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
